1a21608181ac539137a590475a18fc659afd23ec
Kenneth Sylar <verbalsylar@gmail.com>
2022-03-21 10:12:36 +0200

    fixed register route

--- routes/dashboard/account.php
+++ routes/dashboard/account.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('register', [AccountController::class, 'index'])->name('account');
+Route::get('account', [AccountController::class, 'index'])->name('account');
